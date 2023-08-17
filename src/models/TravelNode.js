f34c87c1d6fdc315155b98798e04dcd973ee1ecf
Gabriel Ribeiro <49738156+GabrielSSRibeiro@users.noreply.github.com>
2023-08-16 23:10:27 -0400

    added element logic to node

--- src/models/TravelNode.js
+++ src/models/TravelNode.js
@@ -1,6 +1,9 @@
 module.exports = {
   name: String,
   notes: String,
+  findResourcesDifficulty: Number,
+  materialRarity: Number,
+  isHazardous: Boolean,
   x: Number,
   y: Number,
   angle: Number,
